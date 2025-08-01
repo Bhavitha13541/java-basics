27a7abbfff33677a4a3e8ebd5709533121391420
Bhavitha13541 <bhavitha281reddy@gmail.com>
2025-08-01 21:10:59 +0530

    adding files

---
+++ JavaMethods.java
@@ -0,0 +1,14 @@
+// public class JavaMethods {
+//     public static void sum(int a,int b){
+//         int c;
+//         c = a + b;
+//         System.out.println("Sum of " + a + "and" + b + " is" + c);
+//     }
+//     public static void main(String[] args){
+//         sum(10,20);
+//     }
+// }
+// output:
+// Sum of 10and20 is30
+
+
